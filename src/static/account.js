b6edeca04785dd3993c513e4bc24ea976058b3c0
Oguz <you@example.com>
2024-02-14 21:41:14 +0000

    login&signup page done without the googleAPI

--- src/static/account.js
+++ src/static/account.js
@@ -30,25 +30,38 @@
 const signUpBtn = document.querySelector('#signUp');
 const loginBtn = document.querySelector('#login');
 const action = document.querySelector('#action');
-let forgotPassword = document.querySelector('.forgot_password');
+let forgotPassword = document.querySelector('.forgot-password');
+let headerText = '';
 
-signUpBtn.addEventListener('click', () => {
-    action.textContent = signUpBtn.textContent;
+signUpBtn.addEventListener('click', (event) => {
+    formatHeader(event);
     if (action.textContent) {
         nameInput.style.display = 'flex';
         forgotPassword.style.display = 'none';
         document.getElementById('signUp').classList.add('gray');
         document.getElementById('login').classList.remove('gray');
     }
+    action.textContent = headerText;
 });
 
-loginBtn.addEventListener('click', () => {
-    action.textContent = loginBtn.textContent;
+loginBtn.addEventListener('click', (event) => {
+    formatHeader(event);
     if (action.textContent) {
         nameInput.style.display = 'none';
         forgotPassword.style.display = 'flex';
         document.getElementById('login').classList.add('gray');
         document.getElementById('signUp').classList.remove('gray');
     }
+    action.textContent = headerText;
 })
 
+function formatHeader(event){
+    headerText = event.target.textContent;
+    let words = headerText.split('-');
+
+    for (let i = 0; i < words.length; i++) {
+        words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+    }
+    let formattedString = words.join(' ');
+    headerText = formattedString;
+}
