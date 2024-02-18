ef9994a5f3213426c850fcf7bb65c0a1a101d5d2
Akobir <akobir0929@gamil.com>
2024-02-18 20:29:58 +0000

    beta version for google sign in i cant link index js to html

--- src/static/account.js
+++ src/static/account.js
@@ -1,32 +1,3 @@
-// 'use strict'
-
-// function setAction(action) {
-//     document.getElementById('action').textContent = action;
-//     let nameInput = document.getElementById('nameInput');
-//     let forgotPassword = document.querySelector('.forgot_password');
-
-//     if (action === 'Login') {
-//         nameInput.style.display = 'none';
-//         forgotPassword.style.display = 'flex';
-
-//     } else {
-//         nameInput.style.display = 'flex';
-//         forgotPassword.style.display = 'none';
-//     }
-// }
-
-// document.querySelector('#signUpButton').addEventListener('click', function () {
-//     setAction('Sign Up');
-//     document.getElementById('signUpButton').classList.add('gray');
-//     document.getElementById('loginButton').classList.remove('gray');
-// });
-
-// document.querySelector('#loginButton').addEventListener('click', function () {
-//     setAction('Login');
-//     document.getElementById('loginButton').classList.add('gray');
-//     document.getElementById('signUpButton').classList.remove('gray');
-// });
-
 const signUpBtn = document.querySelector('#signUp');
 const loginBtn = document.querySelector('#login');
 const action = document.querySelector('#action');
