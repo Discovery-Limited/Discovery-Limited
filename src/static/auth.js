a72413a8ac798236ae8c148c8c825d2d32831601
ItzRitz404 <hritiktailor@gmail.com>
2024-02-24 20:14:48 +0000

    Update CSS and authentication code

--- src/static/auth.js
+++ src/static/auth.js
@@ -1,10 +1,10 @@
-const passport = require('passport');
-var GoogleStrategy = require('passport-google-oauth2').Strategy;
+import { use, serializeUser, deserializeUser } from 'passport';
+import { Strategy as GoogleStrategy } from 'passport-google-oauth2';
 
 GOOGLE_CLIENT_ID = '15203078856-u0p12011dkvm9umtere0kcg7csgj24dg.apps.googleusercontent.com';
 GOOGLE_CLIENT_SECRET = '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';
 
-passport.use(new GoogleStrategy({
+use(new GoogleStrategy({
   clientID: GOOGLE_CLIENT_ID,
   clientSecret: GOOGLE_CLIENT_SECRET,
   callbackURL: "http://localhost:8080/google/callback",
@@ -14,10 +14,10 @@ function(request, accessToken, refreshToken, profile, done) {
   return done(null, profile);
 }));
 
-passport.serializeUser(function(user, done) {
+serializeUser(function(user, done) {
   done(null, user);
 });
 
-passport.deserializeUser(function(user, done) {
+deserializeUser(function(user, done) {
   done(null, user);
 });
