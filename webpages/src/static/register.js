e7a561f4b123515e8e43ef3357bd26c8a338bcfa
Oguz <oguz.gur.cs@gmail.com>
2024-03-05 22:01:24 +0000

    register.js added&startup code for pw check

---
+++ webpages/src/static/register.js
@@ -0,0 +1,16 @@
+'use strict';
+
+let pwInfo = document.querySelector('#pw-info');
+let confirmInfo = document.querySelector('#confirm-info');
+
+pwInfo.addEventListener('keydown', passwordHandler());
+
+confirmInfo.addEventListener('keydown', confirmPassword());
+
+function passwordHandler() {
+
+}
+
+function confirmPassword() {
+
+}
