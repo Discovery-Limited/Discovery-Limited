772b388f1697830718bb59ad5d83cd432a8f76d3
Hritik Tailor <hritiktailor@gmail.com>
2024-03-06 22:40:08 +0000

    Refactor register.html and register.js

--- webpages/src/static/register.js
+++ webpages/src/static/register.js
@@ -4,11 +4,11 @@ let pwInfo = document.querySelector('#pw-info');
 let confirmInfo = document.querySelector('#confirm-info');
 
 let password = document.querySelector('#password');
-let confirmPassword = document.querySelector('#confirm');
+let confirmPassword = document.getElementById('confirmpassword');
 
-pwInfo.addEventListener('keydown', passwordHandler());
+// pwInfo.addEventListener('keydown', passwordHandler());
 
-confirmInfo.addEventListener('keydown', confirmPassword());
+// confirmInfo.addEventListener('keydown', confirmPassword());
 
 // function passwordHandler() {
 //     let password = document.querySelector('#password').value;
@@ -25,18 +25,32 @@ confirmInfo.addEventListener('keydown', confirmPassword());
 
 // }
 
-function passwordHandler() {
-    if (password.value.length < 8) {
-        pwInfo.innerHTML = 'Password must be at least 8 characters long';
-    } else {
-        return 'Password is valid';
-    }
-}
+// function passwordHandler() {
+//     if (password.value.length < 8) {
+//         pwInfo.innerHTML = 'Password must be at least 8 characters long';
+//     } else {
+//         return 'Password is valid';
+//     }
+// }
 
-function confirmPassword() {
-    if (password.value !== confirmPassword.value) {
-        confirmInfo.innerHTML = 'Passwords do not match';
+// function confirmPassword() {
+//     if (password.value !== confirmPassword.value) {
+//         confirmInfo.innerHTML = 'Passwords do not match';
+//     } else {
+//         password.disabled = enabled;
+//     }
+// }
+
+// let password = document.getElementById('password');
+
+confirmPassword.addEventListener('keyup', validatePassword());
+
+function validatePassword() {
+    if (password != confirmPassword) {
+        confirmInfo.classList.toggle('hidden');
+        confirmInfo.textContent = '${Passwords do not match}';
     } else {
-        password.disabled = enabled;
+        confirmInfo.classList.toggle('hidden');
+        confirmInfo.textContent = '${Passwords match}';
     }
 }
