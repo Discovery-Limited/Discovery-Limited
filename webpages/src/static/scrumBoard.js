6cd5b1463e0e9e579fac8796f5c6e404d71da9e0
David Tran <david_02_@hotmail.co.uk>
2024-03-22 21:16:54 +0000

    Tasks can be dragged to other columns

--- webpages/src/static/scrumBoard.js
+++ webpages/src/static/scrumBoard.js
@@ -1,123 +1,172 @@
-var LocalStorage = function () {
-    function set(key, val) {
-        localStorage.setItem(key, val);
-    }
+// var LocalStorage = function () {
+//     function set(key, val) {
+//         localStorage.setItem(key, val);
+//     }
 
-    function get(key) {
-        return localStorage.getItem(key);
-    }
+//     function get(key) {
+//         return localStorage.getItem(key);
+//     }
 
-    function remove(key) {
-        localStorage.removeItem(key);
-    }
+//     function remove(key) {
+//         localStorage.removeItem(key);
+//     }
 
-    return {
-        set: set,
-        get: get,
-        remove: remove
-    }
-}();
+//     return {
+//         set: set,
+//         get: get,
+//         remove: remove
+//     }
+// }();
 
-var Helper = function () {
-    function init() {
-        checkIfEmptyAssigned();
-    }
+// var Helper = function () {
+//     function init() {
+//         checkIfEmptyAssigned();
+//     }
 
-    function checkAssigned() {
-        Handlebars.registerHelper('checkIfEmptyAssigned', function (val, options) {
-            if (val) {
-                return val;
-            } else {
-                return "Unassigned";
-            }
-        });
-    }
+//     function checkAssigned() {
+//         Handlebars.registerHelper('checkIfEmptyAssigned', function (val, options) {
+//             if (val) {
+//                 return val;
+//             } else {
+//                 return "Unassigned";
+//             }
+//         });
+//     }
 
-    return {
-        init: init
-    }
-}();
+//     return {
+//         init: init
+//     }
+// }();
 
-Helper.init();
+// Helper.init();
 
 
 
-var App = function () {
-    function init() {
-        draggable();
-        droppable();
-        openCard();
-        createTask();
-        closeModal();
-        printNotes();
-    }
+// var App = function () {
+//     function init() {
+//         draggable();
+//         droppable();
+//         openCard();
+//         createTask();
+//         closeModal();
+//         printNotes();
+//     }
 
-    function createTask() {
-        var source = $("#task-card-template").html();
-        var template = Handlebars.compile(source);
-        var task = document.getElementById('add-task');
+//     function createTask() {
+//         var source = $("#task-card-template").html();
+//         var template = Handlebars.compile(source);
+//         var task = document.getElementById('add-task');
 
-        $("#add-task").on("click", function (e) {
-            e.preventDefault();
-            $("#add-task-modal").removeClass("hide");
-            $(".content").addClass("hide");
+//         $("#add-task").on("click", function (e) {
+//             e.preventDefault();
+//             $("#add-task-modal").removeClass("hide");
+//             $(".content").addClass("hide");
 
-            $('#add-task-modal').find('form').on('submit', function (e) {
-                e.preventDefault();
-                var obj = {};
-                var params = $(this).serialize();
-                var splitParams = params.split('&');
+//             $('#add-task-modal').find('form').on('submit', function (e) {
+//                 e.preventDefault();
+//                 var obj = {};
+//                 var params = $(this).serialize();
+//                 var splitParams = params.split('&');
 
-                for (var i = 0, l = splitParams.length; i < l; i++) {
-                    var keyVal = splitParams[i].split('=');
-                    obj[keyVal[0]] = unescape(keyVal[1]);
-                }
+//                 for (var i = 0, l = splitParams.length; i < l; i++) {
+//                     var keyVal = splitParams[i].split('=');
+//                     obj[keyVal[0]] = unescape(keyVal[1]);
+//                 }
 
-                // TODO: Add validations
-                if (obj.description === '' || obj.title === '') {
-                    return;
-                }
+//                 // TODO: Add validations
+//                 if (obj.description === '' || obj.title === '') {
+//                     return;
+//                 }
 
-                var iid = LocalStorage.get('taskCounter');
-                obj.id = ++iid;
-                obj.status = 'pending';
-                LocalStorage.set('task-' + obj.id, JSON.stringify(obj));
-                LocalStorage.set('taskCounter', iid);
+//                 var iid = LocalStorage.get('taskCounter');
+//                 obj.id = ++iid;
+//                 obj.status = 'pending';
+//                 LocalStorage.set('task-' + obj.id, JSON.stringify(obj));
+//                 LocalStorage.set('taskCounter', iid);
 
-                var newCard = template([obj]);
-                $('#dashboard #' + obj.status).append(newCard);
-                draggable();
+//                 var newCard = template([obj]);
+//                 $('#dashboard #' + obj.status).append(newCard);
+//                 draggable();
 
-                $('.close-modal').trigger('click');
+//                 $('.close-modal').trigger('click');
 
-                //Clear form fields after submit
-                $(this).find('input[type=text], textarea').val('');
-            });
+//                 //Clear form fields after submit
+//                 $(this).find('input[type=text], textarea').val('');
+//             });
 
-        });
-    }
+//         });
+//     }
 
-    function closeModal() {
-        $('.close-modal').on('click', function () {
-            $('.modal').addClass('hide');
-        });
-    }
+//     function closeModal() {
+//         $('.close-modal').on('click', function () {
+//             $('.modal').addClass('hide');
+//         });
+//     }
 
-    function draggable() {
-        $(".card").draggable({
-            handle: 'h5',
-            revert: false,
-            helper: function (e) {
-                var original = $(e.target).hasClass("ui-draggable") ? $(e.target) : $(e.target).closest(".ui-draggable");
-                return original.clone().css({
-                    width: original.width()
-                });
-            },
-            scroll: false,
-            cursor: "move",
-            start: function (event, ui) {},
-            stop: function(event, ui) {}
-        });
-    }
-}
+//     function draggable() {
+//         $(".card").draggable({
+//             handle: 'h5',
+//             revert: false,
+//             helper: function (e) {
+//                 var original = $(e.target).hasClass("ui-draggable") ? $(e.target) : $(e.target).closest(".ui-draggable");
+//                 return original.clone().css({
+//                     width: original.width()
+//                 });
+//             },
+//             scroll: false,
+//             cursor: "move",
+//             start: function (event, ui) {},
+//             stop: function(event, ui) {}
+//         });
+//     }
+// }
+
+
+// ---------- Drag Tasks ----------
+const draggables = document.querySelectorAll(".task");
+const droppables = document.querySelectorAll(".column");
+
+draggables.forEach((task) => {
+    task.addEventListener("dragstart", () => {
+        task.classList.add("is-dragging");
+    });
+    task.addEventListener("dragend", () => {
+        task.classList.remove("is-dragging");
+    });
+});
+
+droppables.forEach((zone) => {
+    zone.addEventListener("dragover", (e) => {
+        e.preventDefault();
+
+        const bottomTask = insertAboveTask(zone, e.clientY);
+        const curTask = document.querySelector(".is-dragging");
+
+        if (!bottomTask) {
+            zone.appendChild(curTask);
+        } else {
+            zone.insertBefore(curTask, bottomTask);
+        }
+    });
+});
+
+const insertAboveTask = (zone, mouseY) => {
+    const els = zone.querySelectorAll(".task:not(.is-dragging)");
+
+    let closestTask = null;
+    let closestOffset = Number.NEGATIVE_INFINITY;
+
+    els.forEach((task) => {
+        const { top } = task.getBoundingClientRect();
+
+        const offset = mouseY - top;
+
+        if (offset < 0 && offset > closestOffset) {
+            closestOffset = offset;
+            closestTask = task;
+        }
+    });
+
+    return closestTask;
+};
 
