ece9497f12215bb40891054993d63231f4f99e06
Hritik Tailor <hritiktailor@gmail.com>
2024-04-24 17:36:24 +0100

    added check for username and email and removed pwInfo password invalid statement

--- webpages/src/static/register.js
+++ webpages/src/static/register.js
@@ -6,6 +6,9 @@
 
 document.addEventListener("DOMContentLoaded", () => {
 
+    const username = document.querySelector('#username');
+    const email = document.querySelector('#email');
+
     const pwInfo = document.querySelector('#pw-info');
     const confirmInfo = document.querySelector('#confirm-info');
 
@@ -13,9 +16,15 @@ document.addEventListener("DOMContentLoaded", () => {
     const confirmPassword = document.querySelector('#confirmpassword');
 
     const button = document.querySelector('.submit-form');
+    let emailCheck = false; 
+    let usernameCheck = false;  
 
-    confirmPassword.addEventListener('keyup', validatePassword);
+    confirmPassword.addEventListener('keyup', validateInfo);
     password.addEventListener('keyup', passwordHandler);
+    
+    username.addEventListener('keydown', validateInfo);
+    email.addEventListener('keydown', validateInfo);
+
     // I changed the textContent of confirmInfo inside the HTML file so only thing that we had to do is
     // toggling the hidden class:
 
@@ -25,27 +34,43 @@ document.addEventListener("DOMContentLoaded", () => {
             pwInfo.textContent = 'Password must be at least 8 characters long';
         } else if (password.value == null) {
             pwInfo.classList.add('hidden');
-        }
-        else {
-            pwInfo.textContent = 'Password is valid';
+        } else {
             button.style.cursor = 'not-allowed';
         }
     }
 
-    function validatePassword() {
+    // function checkInfo() {
+    //     if (username.value.length <= 0 && email.value.length <= 0) {
+    //         // emailCheck = false;
+    //         // usernameCheck = false;
+    //         button.style.cursor = 'not-allowed';
+    //         button.style.opacity = (0.4);
+    //     } else {
+    //         console.log(email.value);
+    //         // emailCheck = true;
+    //         // usernameCheck = true;
+    //         button.style.opacity = (1);
+    //         button.style.cursor = 'pointer';
+    //     }
+    // }
+
+    function validateInfo() {
+        // console.log(email.value);
         if (password.value != confirmPassword.value) {
             // .value means the value of the input which is better way to track the input values
             confirmInfo.classList.remove('hidden');
             confirmInfo.textContent = 'Passwords do not match';
             button.style.cursor = 'not-allowed';
             button.style.opacity = (0.4);
+        } else if (username.value.length <= 0 && email.value.length <= 0) {
+            button.style.cursor = 'not-allowed';
+            button.style.opacity = (0.4);
         } else {
             confirmInfo.classList.add('hidden');
-            confirmInfo.textContent = 'Passwords match';
+            // confirmInfo.textContent = 'Passwords match';
             button.disabled = false;
             button.style.opacity = (1);
             button.style.cursor = 'pointer';
-
         }
         // here's more simplified and efficient way to do it:
         // confirmInfo.classList.toggle('hidden', password.value !== confirmPassword.value); 
