557568346d0c1ae79756b5b3d435811bb5371475
ItzRitz404 <hritiktailor@gmail.com>
2024-04-28 20:12:47 +0100

    Update scrumBoard.html and scrumBoard.js with new task input functionality

--- webpages/src/static/scrumBoard.js
+++ webpages/src/static/scrumBoard.js
@@ -117,3 +117,22 @@ form.addEventListener("submit", (e) => {
 
 });
 
+document.addEventListener("DOMContentLoaded", () => {
+    const addTaskBtn = document.getElementById("add-task-btn");
+    const taskInput = document.getElementById("task");
+
+    taskInput.addEventListener('keyup', checkTaskInput);
+
+    function checkTaskInput() {
+        if (taskInput.value.length == 0) {
+            addTaskBtn.style.cursor = 'not-allowed';
+            addTaskBtn.style.opacity = (0.4);
+        } else if (taskInput.value <= '') {
+            addTaskBtn.style.cursor = 'not-allowed';
+            addTaskBtn.style.opacity = (0.4);
+        } else {
+            addTaskBtn.style.cursor = 'pointer';
+            addTaskBtn.style.opacity = (1);
+        }
+    }
+})
