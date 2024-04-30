f9deffccfb7cc5911fe19440ff3fae6874357f73
abdukabirov <128067872+aabdukabirov@users.noreply.github.com>
2024-04-30 19:40:40 +0100

    fixed scrum scrumBoard

--- webpages/src/static/scrumBoard.js
+++ webpages/src/static/scrumBoard.js
@@ -111,39 +111,37 @@ form.addEventListener("submit", (e) => {
     newTask.classList.remove("is-dragging");
   });
 
-    // todoLane.appendChild(newTask);
-    backlogLane.appendChild(newTask);
-    taskInput.value = "";
-    // addTaskModal.classList.add("hide");
-    window.location.href = "#";
-
-});
-
-document.addEventListener("DOMContentLoaded", () => {
-    const addTaskBtn = document.getElementById("add-task-btn");
-    const taskInput = document.getElementById("task");
-
-    taskInput.addEventListener('keyup', checkTaskInput);
-
-    function checkTaskInput() {
-        if (taskInput.value.length == 0) {
-            addTaskBtn.style.cursor = 'not-allowed';
-            addTaskBtn.style.opacity = (0.4);
-        } else if (taskInput.value <= '') {
-            addTaskBtn.style.cursor = 'not-allowed';
-            addTaskBtn.style.opacity = (0.4);
-        } else {
-            addTaskBtn.style.cursor = 'pointer';
-            addTaskBtn.style.opacity = (1);
-        }
-    }
-})
   // todoLane.appendChild(newTask);
   backlogLane.appendChild(newTask);
   taskInput.value = "";
   // addTaskModal.classList.add("hide");
   window.location.href = "#";
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const addTaskBtn = document.getElementById("add-task-btn");
+  const taskInput = document.getElementById("task");
+
+  taskInput.addEventListener("keyup", checkTaskInput);
+
+  function checkTaskInput() {
+    if (taskInput.value.length == 0) {
+      addTaskBtn.style.cursor = "not-allowed";
+      addTaskBtn.style.opacity = 0.4;
+    } else if (taskInput.value <= "") {
+      addTaskBtn.style.cursor = "not-allowed";
+      addTaskBtn.style.opacity = 0.4;
+    } else {
+      addTaskBtn.style.cursor = "pointer";
+      addTaskBtn.style.opacity = 1;
+    }
+  }
+});
+// todoLane.appendChild(newTask);
+backlogLane.appendChild(newTask);
+taskInput.value = "";
+// addTaskModal.classList.add("hide");
+window.location.href = "#";
 
 document.addEventListener("click", (e) => {
   const expandButton = e.target.closest(".expand-details-btn");
